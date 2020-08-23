a67e46775e7a7582447f0eb4b4059638d85e6a7d
tobymould <toby.mould@icloud.com>
2020-08-23 14:52:20 +0100

    added initial routes for GET/POST/PUT/DELETE

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(400).json({ success: true, msg: 'show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(400).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(400).json({ success: true, msg: 'create new bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(400).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(400).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode of port ${PORT}`));
