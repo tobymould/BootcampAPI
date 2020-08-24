bf59d35a7fbdaee20d71727c0eb63f3b0deae7ad
tobymould <toby.mould@icloud.com>
2020-08-24 11:38:32 +0100

    Connected to mongoDB Atlas collection, implemented global error handler if no connection to DB made to auto-shutdown the app, and implemented 'colors' package which allows clearer console.log displays with clear colour-styling

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,4 +25,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode of port ${PORT}`));
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode of port ${PORT}`.yellow.bold));
+
+// handle Unhandled Promise Rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
