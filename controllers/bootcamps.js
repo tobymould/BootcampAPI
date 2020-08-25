380d89e70bc11ec1485db6ddab78351f3ddfa9bc
tobymould <toby.mould@icloud.com>
2020-08-25 14:13:24 +0100

    Implemented Query String filtering of response results based on query string parameters parsed in using mongoDB operators to return entire documents but that meet a user-specified criteria, Selection of only specific fields from a document to return, and Sorting order they should be arranged in on response to client

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,12 +2,48 @@ const ErrorResponse = require('../utils/errorReponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
+const { estimatedDocumentCount } = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  //Copy req.query
+  const reqQuery = { ...req.query };
+
+  // fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  //Create query String
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt/$gte, etc.)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // SELECT FIELDS:
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Executing query
+  const bootcamps = await query;
 
   res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 });
