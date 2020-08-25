784bec270f61ed7fc0bc1f3c1886a38d22d34c73
tobymould <toby.mould@icloud.com>
2020-08-25 14:53:02 +0100

    implemented ability to limit and added pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,7 +14,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
@@ -42,10 +42,36 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Executing query
   const bootcamps = await query;
 
-  res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+  // pagenation result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit: limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit: limit
+    };
+  }
+
+  res.status(200).json({ success: true, count: bootcamps.length, pagination: pagination, data: bootcamps });
 });
 
 // @desc    Get single bootcamp
