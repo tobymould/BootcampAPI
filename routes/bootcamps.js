baf5071f02a360a81799bf9173fa8c4e4ad4f48d
tobymould <toby.mould@icloud.com>
2020-08-25 17:05:49 +0100

    implemented the courses Routes & Controller files, and added a Resource Router for something related to courses but contained bootcamp nearer the root of the url

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps');
+
+// INCLUDE OTHER RESOURCE ROUTERS
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// RE-ROUTE INTO OTHER RESOURCE ROUTERS
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
