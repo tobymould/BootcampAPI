9115786d3be10a8465db2d1c06f2fb085d6f6dba
tobymould <toby.mould@icloud.com>
2020-08-28 09:22:41 +0100

    transferred the advanced search middleware from 'getAllBootcamps' controller to a separate middleware and generalised it to work on all route/controller types

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,70 +9,7 @@ const { estimatedDocumentCount } = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-
-  //Copy req.query
-  const reqQuery = { ...req.query };
-
-  // fields to exclude
-  const removeFields = ['select', 'sort', 'page', 'limit'];
-
-  // Loop over removeFields and delete them from reqQuery
-  removeFields.forEach(param => delete reqQuery[param]);
-
-  //Create query String
-  let queryStr = JSON.stringify(reqQuery);
-
-  // Create operators ($gt/$gte, etc.)
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-
-  // finding resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
-
-  // SELECT FIELDS:
-  if (req.query.select) {
-    const fields = req.query.select.split(',').join(' ');
-    query = query.select(fields);
-  }
-
-  //Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  // Pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  // Executing query
-  const bootcamps = await query;
-
-  // pagenation result
-  const pagination = {};
-
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit: limit
-    };
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit: limit
-    };
-  }
-
-  res.status(200).json({ success: true, count: bootcamps.length, pagination: pagination, data: bootcamps });
+  res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get single bootcamp
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // INCLUDE OTHER RESOURCE ROUTERS
 const courseRouter = require('./courses');
 
@@ -13,7 +16,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
 module.exports = router;
