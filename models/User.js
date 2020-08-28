8bfe4c4e9498001e26321cde0bbbcde7263c82e8
tobymould <toby.mould@icloud.com>
2020-08-28 10:49:45 +0100

    signing with JSON web tokens and getting the JWT in the response implemeneted

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,5 +31,18 @@ const UserSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
