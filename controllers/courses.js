75548b7bc28740e892e524b2203d64353d8a87a2
tobymould <toby.mould@icloud.com>
2020-08-28 21:35:09 +0100

    implemented course ownership too - only owners of the bootcamp can add courses to them.

--- controllers/courses.js
+++ controllers/courses.js
@@ -45,11 +45,17 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  PRIVATE
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
     return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorised to add a course to bootcamp ${bootcamp._id}`, 401));
   }
 
   const course = await Course.create(req.body);
@@ -68,6 +74,11 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  // Make sure user is bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorised to update course ${course._id}`, 401));
   }
 
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
@@ -89,6 +100,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  // Make sure user is bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorised to delete course ${course._id}`, 401));
   }
 
   await course.remove();
